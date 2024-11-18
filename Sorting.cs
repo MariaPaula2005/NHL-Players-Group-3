57b7fe0d1bbcac12af448c80b4ea64a9c54683b8
Felipe Mora <112905298+felipemora69@users.noreply.github.com>
2024-11-18 10:12:01 -0700

    Add the sorting file

---
+++ Sorting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NHL_Players
+{
+    public class Sorting
+    {
+        public List<Player> Sort(List<Player> players, string field, bool ascending)
+        {
+            var property = typeof(Player).GetProperty(field);
+            if (property == null) return players;
+
+            return ascending
+                ? players.OrderBy(p => property.GetValue(p)).ToList()
+                : players.OrderByDescending(p => property.GetValue(p)).ToList();
+        }
+}}
