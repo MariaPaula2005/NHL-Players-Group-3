2427d8ff56b57e67ae64470f8a37b72549143219
Yuvraj Sidhu <147128567+yuvi23-sid@users.noreply.github.com>
2024-11-19 01:32:03 -0700

    Add files via upload

---
+++ NHL Players Update/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHL_Players
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Team { get; set; }
+        public string Pos { get; set; }
+        public int GP { get; set; }
+        public int G { get; set; }
+        public int A { get; set; }
+        public int P { get; set; }
+        public int PlusMinus { get; set; }
+        public int PIM { get; set; }
+        public double PGP { get; set; }
+        public int PPG { get; set; }
+        public int PPP { get; set; }
+        public int SHG { get; set; }
+        public int SHP { get; set; }
+        public int GWG { get; set; }
+        public int OTG { get; set; }
+        public int S { get; set; }
+        public double SPercentage { get; set; }
+        public string TOIGP { get; set; }
+        public double ShiftsGP { get; set; }
+        public double FOWPercentage { get; set; }
+    }
+}
